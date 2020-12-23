e927eb5e90bbf69f1d5b476048cc8cb84afe03b2
roschel <joaomroschel@gmail.com>
2020-12-23 14:18:46 -0300

    Login page style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <h1>hello world</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
