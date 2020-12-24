85f59103d913853fc502041bc6be5b185027dc92
roschel <joaomroschel@gmail.com>
2020-12-23 21:47:35 -0300

    toast structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
-import { AuthProvider } from './context/AuthContext';
+import ToastContainer from './components/ToastContainer';
+import { AuthProvider } from './hooks/AuthContext';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToastContainer />
     <GlobalStyle />
   </>
 );
